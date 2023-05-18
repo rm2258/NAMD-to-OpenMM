05f2856c50db41feb37f25115c963ecf31333fd8
U-MSI\ramon <ramendoza@uchicago.edu>
2023-05-18 12:56:16 -0800

    removed help() functionality

--- libraries/namdextractor.py
+++ libraries/namdextractor.py
@@ -10,8 +10,24 @@ import numpy as np
 
 class NamdEnergyExtractor:
     """
-    Add a step to time converter in this class as well
-    will need to import matplotlib.pyplot as plt
+    class namdextractor.NamdEnergyExtractor(namd_output_file)
+                         This module is for extracting energy data from NAMD output files
+                         __init__(self, namd_output_file)
+                         Parameters: 
+                             namd_output_file (str) - Name of NAMD log file
+                         Attributes:
+                             namd_output_file: NAMD log file
+                             data: Dictionary where keys are the ETITLE entries and values are the energies
+                             keys: List of ETITLE headers
+                         Methods
+                             etitle(self)
+                                 Get ETITLE from NAMD log file
+                                 Returns: A dictionary where keys are the ETITLE entries and 
+                                          values are empty lists
+                             energy_extractor(self)
+                                 Extract energies from NAMD log file
+                                 Returns: A numpy array containing potential energies from NAMD 
+                                          log file
     """
 
     def __init__(self, namd_output_file):
@@ -24,37 +40,9 @@ class NamdEnergyExtractor:
         self.keys = ['ETITLE:', 'TS', 'BOND', 'ANGLE', 'DIHED', 'IMPRP', 'ELECT', 'VDW', 'BOUNDARY', 'MISC', 'KINETIC',
                      'TOTAL', 'TEMP', 'POTENTIAL', 'TOTAL3', 'TEMPAVG', 'PRESSURE', 'GPRESSURE', 'VOLUME', 'PRESSAVG',
                      'GPRESSAVG']
-        print('Use the help() method for class use')
-
-    @staticmethod
-    def help():
-        help_message = 'class namdextractor.NamdEnergyExtractor(namd_output_file)\n' + \
-                       '  This module is for extracting energy data from NAMD output files\n\n' + \
-                       '  __init__(self, namd_output_file)\n' + \
-                       '    Parameters: namd_output_file (str) - Name of NAMD log file\n\n' + \
-                       '  Attributes\n' + \
-                       '    namd_output_file\n' + \
-                       '      NAMD log file\n\n' + \
-                       '    data\n' + \
-                       '      Dictionary where keys are the ETITLE entries and values are the energies\n\n' + \
-                       '   keys\n' + \
-                       '     List of ETITLE headers\n\n' + \
-                       '  Methods\n' + \
-                       '    help()\n' + \
-                       '      Print a help message\n\n' + \
-                       '      Returns: A help message\n\n' + \
-                       '    etitle(self)\n' + \
-                       '      Get ETITLE from NAMD log file\n\n' + \
-                       '      Returns: A dictionary where keys are the ETITLE entries and values are\n' + \
-                       '               empty lists\n\n' + \
-                       '    energy_extractor(self)\n' + \
-                       '      Extract energies from NAMD log file\n\n' + \
-                       '      Returns: A numpy array containing potential energies from NAMD log file'
-        print(help_message)  # Decide if you want to keep this as return
 
     def etitle(self):
-        """
-        Use in case log file formatting Changes
+        """Returns the headers of a log file
         """
         with open(self.namd_output_file, 'r') as f:
             lines = f.readlines()
@@ -68,6 +56,8 @@ class NamdEnergyExtractor:
         return data
 
     def energy_extractor(self):
+        """Returns the energies for each of the headers
+        """
         with open(self.namd_output_file, 'r') as f:
             lines = f.readlines()
             for line in lines:
