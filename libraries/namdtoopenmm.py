236c0c2cb9dbef5d698ec66010e42969f2f62ec6
U-MSI\ramon <ramendoza@uchicago.edu>
2023-05-18 14:10:01 -0800

    Removed help()

--- libraries/namdtoopenmm.py
+++ libraries/namdtoopenmm.py
@@ -13,6 +13,48 @@ import numpy as np
 
 
 class NamdToOpenmmTools:
+    """
+    class namdtoopenmm.NamdToOpenmmTools(trajectory_file, psf_file, parameter_files, 
+                                         temperature, pbc)
+        This module is for computing 'NAMD'-like energies in OpenMM
+        __init__(self, trajectory_file, psf_file, parameter_files, temperature, pbc)
+        Parameters: trajectory_file (str) - File name of DCD trajectory file
+                    psf_file (str) - File name of PSF file
+                    parameter_files (list) - list of parameter files to load
+                    temperature (float) - simulation temperature in kelvin\
+                    pbc (tuple) - a, b, c for periodic boundary conditions in angstroms
+        Attributes: trajectory_file - Trajectory file to load
+                    psf_file - Protein Structure File (psf) to load
+                    parameter_files - A list of parameter files to load
+                    temperature - The simulation temperature in kelvin
+                    pbc - a, b, c for periodic boundary conditions in angstroms
+                    time_conversions - Time conversion constants
+                    trajectory - Loaded trajectory file as an mdtraj.Trajectory object
+                    system - An OpenMM system object
+                    simulation - Instantiate an OpenMM simulation object
+        Methods
+            load_trajectory(self)
+                Load a trajectory file
+                Returns: An mdtraj.Trajectory object
+                
+            load_system(self, parameter_files, temperature, pbc)
+                Makes an OpenMM simulation object
+                Parameters: parameter_files (list) - list of parameter files to load
+                            temperature (float) - simulation temperature in kelvin
+                            pbc (tuple) - a, b, c for periodic boundary conditions in angstroms
+                Returns: system, simulation
+                         system - An OpenMM system object
+                         simulation - An OpenMM simulation object
+            
+            compute_energies(self, dcdfreq, time='femtoseconds', print_output=False)
+                Computes 'NAMD'-like potential energies in OpenMM
+                    Parameters: dcdfreq (int) - DCD frequency output
+                                time (str) - Desired unit of time
+                                print_output (bool) - Print energy breakdown output
+                    Returns: potential_energies, output
+                             potential_energies (numpy array) - Potential energies
+                             output (list) - List of strings of energy breakdown output
+    """
 
     def __init__(self, trajectory_file, psf_file, parameter_files, temperature, pbc):
         self.trajectory_file = str(trajectory_file)
@@ -20,93 +62,57 @@ class NamdToOpenmmTools:
         self.parameter_files = list(parameter_files)
         self.temperature = float(temperature)
         self.pbc = tuple(pbc)
-        self.time_conversions = {'femtoseconds': 2, 'picoseconds': 0.002, 'nanoseconds': 2 * 10 ** (-6),
-                                 'microseconds': 2 * 10 ** (-9)}
+        self.time_conversions = {'femtoseconds': 2, 'picoseconds': 0.002, 'nanoseconds': 2 
+                                 * 10 ** (-6), 'microseconds': 2 * 10 ** (-9)}
         self.trajectory = self.load_trajectory()
-        self.system, self.simulation = self.load_system(self.parameter_files, self.temperature, self.pbc)
-        print('Use the help() method for class use')
+        self.system, self.simulation = self.load_system(self.parameter_files, self.temperature, 
+                                                        self.pbc)
 
-    @staticmethod
-    def help():
-        help_message = 'class namdtoopenmm.NamdToOpenmmTools(trajectory_file, psf_file, parameter_files, temperature,' \
-                       ' pbc)\n' + \
-                       '  This module is for computing \'NAMD\'-like energies in OpenMM\n\n' + \
-                       '  __init__(self, trajectory_file, psf_file, parameter_files, temperature, pbc)\n' + \
-                       '    Parameters: trajectory_file (str) - File name of DCD trajectory file\n' + \
-                       '                psf_file (str) - File name of PSF file\n' + \
-                       '                parameter_files (list) - list of parameter files to load\n' + \
-                       '                temperature (float) - simulation temperature in kelvin\n' + \
-                       '                pbc (tuple) - a, b, c for periodic boundary conditions in angstroms\n\n' + \
-                       '  Attributes\n' + \
-                       '    trajectory_file\n' + \
-                       '      Trajectory file to load\n\n' + \
-                       '    psf_file\n' + \
-                       '      Protein Structure File (psf) to load\n\n' + \
-                       '    parameter_files\n' + \
-                       '      A list of parameter files to load\n\n' + \
-                       '    temperature\n' + \
-                       '      The simulation temperature in \n\n' + \
-                       '    pbc\n' + \
-                       '      a, b, c for periodic boundary conditions in angstroms\n\n' + \
-                       '    time_conversions\n' + \
-                       '      Time conversion constants\n\n' + \
-                       '    trajectory\n' + \
-                       '      Loaded trajectory file as an mdtraj.Trajectory object\n\n' + \
-                       '   system\n' + \
-                       '     An OpenMM system object\n\n' + \
-                       '    simulation\n' + \
-                       '      Instantiate an OpenMM simulation object\n\n' + \
-                       '  Methods\n' + \
-                       '    help()\n' + \
-                       '      Print a help message\n\n' + \
-                       '      Returns: A help message\n\n' + \
-                       '    load_trajectory(self)\n' + \
-                       '      Load a trajectory file\n\n' + \
-                       '      Returns: An mdtraj.Trajectory object\n\n' + \
-                       '    load_system(self, parameter_files, temperature, pbc)\n' + \
-                       '      Makes an OpenMM simulation object\n\n' + \
-                       '      Parameters: parameter_files (list) - list of parameter files to load\n' + \
-                       '                  temperature (float) - simulation temperature in kelvin\n' + \
-                       '                  pbc (tuple) - a, b, c for periodic boundary conditions in angstroms\n' + \
-                       '      Returns: system, simulation\n' + \
-                       '               system - An OpenMM system object\n' + \
-                       '               simulation - An OpenMM simulation object\n\n' + \
-                       '    compute_energies(self, dcdfreq, time=\'femtoseconds\', print_output=False)\n' + \
-                       '      Computes \'NAMD\'-like potential energies in OpenMM\n\n' + \
-                       '      Parameters: dcdfreq (int) - DCD frequency output\n' + \
-                       '                  time (str) - Desired unit of time\n' + \
-                       '                  print_output (bool) - Print energy breakdown output\n' + \
-                       '      Returns: potential_energies, output\n' + \
-                       '               potential_energies (numpy array) - Potential energies\n' + \
-                       '               output (list) - List of strings of energy breakdown output\n\n'
-        print(help_message)
 
     def load_trajectory(self):
+        """Returns an mdtraj.Trajectory object from a trajectory file
+        """
         traj = md.load_dcd(self.trajectory_file, self.psf_file)
         return traj
 
     def load_system(self, parameter_files, temperature, pbc):
+        """Returns the OpenMM system and simulation object
+        """
         psf = CharmmPsfFile(self.psf_file)
         params = CharmmParameterSet(*parameter_files)
         psf.setBox(pbc[0] * angstroms, pbc[1] * angstroms, pbc[2] * angstroms)
-        system = psf.createSystem(params, nonbondedMethod=PME, nonbondedCutoff=1.2 * nanometers,
-                                  switchDistance=1.0 * nanometers, constraints=HBonds, ewaldErrorTolerance=0.0005)
+        system = psf.createSystem(params, nonbondedMethod=PME, nonbondedCutoff=1.2 
+                                  * nanometers, switchDistance=1.0 * nanometers,
+                                  constraints=HBonds, ewaldErrorTolerance=0.0005)
         pressure_controller = MonteCarloMembraneBarostat
-        system.addForce(pressure_controller(1.01325 * bar, 0.0 * bar * nanometers, temperature * kelvin,
-                                            pressure_controller.XYIsotropic, pressure_controller.ZFree, 100))
+        system.addForce(pressure_controller(1.01325 * bar, 0.0 * bar * nanometers, temperature 
+                                            * kelvin, pressure_controller.XYIsotropic,
+                                            pressure_controller.ZFree, 100))
         for i, f in enumerate(system.getForces()):
             f.setForceGroup(i)
-        integrator = LangevinIntegrator(temperature * kelvin, 1 / picosecond, 0.002 * picoseconds)
-        try:
+        integrator = LangevinIntegrator(temperature * kelvin, 1 / picosecond, 0.002 
+                                        * picoseconds)
+        _cudaPlatform = False
+        _numPlatforms = Platform.getNumPlatforms()
+        for i in range(_numPlatforms):
+            _namePlatform = Platform.getPlatform(i).getName()
+            if _namePlatform is 'CUDA':
+                _cudaPlatform = True
+                break
+        
+        if _cudaPlatform:
             platform = Platform.getPlatformByName('CUDA')
             prop = {'Precision': 'single'}
             simulation = Simulation(psf.topology, system, integrator, platform, prop)
-        except OpenMMException:
-            platform = Platform.getPlatformByName('CPU')
+        else:
+            platform = Platform.getPlatformByName('CPU') # The cpu platform also has no. of
+                                                         # threads property could add this
             simulation = Simulation(psf.topology, system, integrator)
         return system, simulation
 
     def compute_energies(self, dcdfreq, time='femtoseconds', print_output=False):
+        """Returns the energies computed in OpenMM in a 'NAMD'-like output
+        """
         potential_energies = np.array([])
         output = []
         if time not in self.time_conversions:
